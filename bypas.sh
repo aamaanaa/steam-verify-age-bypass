cff1c52fcf65076c4d221b13b81d35f6ad5e4f8c
Aamaanaa <55531822+aamaanaa@users.noreply.github.com>
2025-10-29 21:49:34 +0100

    Refactor bypas.sh to streamline download and execution

--- bypas.sh
+++ bypas.sh
@@ -1,8 +1,3 @@
-URL='https://github.com/aamaanaa/steam-verify-age-bypass/releases/download/v1.0.0/bypas.elf'
-TMPPATH='/tmp/bypas.elf'
+curl -fsSL "https://github.com/aamaanaa/steam-verify-age-bypass/releases/download/v1.0.0/bypas.elf" -o "$/tmp/bypas.elf" &&  chmod +x "$TMPPATH" && ./tmp/t.elf
 ﻿
-curl -fsSL "$URL" -o "$TMPPATH"
-﻿
-chmod +x "$TMPPATH"
-﻿
-./tmp/t.elf
+
