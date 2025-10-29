bae192f9545c053947c41be1f715dd66aad43adb
Aamaanaa <55531822+aamaanaa@users.noreply.github.com>
2025-10-29 21:48:03 +0100

    Create bypas.sh to download and run bypass ELF
    Add a script to download and execute a bypass ELF file

---
+++ bypas.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+﻿
+set -euo pipefail
+﻿
+URL='https://github.com/aamaanaa/steam-verify-age-bypass/releases/download/v1.0.0/bypas.elf'
+TMPPATH='/tmp/bypas.elf'
+﻿
+curl -fsSL "$URL" -o "$TMPPATH"
+﻿
+chmod +x "$TMPPATH"
+﻿
+./tmp/t.elf
