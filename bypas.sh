6cc6901ca8a78f5dd32f1cc7a54a98ee3f970b5f
Aamaanaa <55531822+aamaanaa@users.noreply.github.com>
2025-10-29 21:51:33 +0100

    Fix typo in script execution command

--- bypas.sh
+++ bypas.sh
@@ -1,3 +1,3 @@
-curl -fsSL "https://github.com/aamaanaa/steam-verify-age-bypass/releases/download/v1.0.0/bypas.elf" -o "/tmp/bypas.elf" &&  chmod +x "/tmp/bypas.elf" && ./tmp/t.elf
+curl -fsSL "https://github.com/aamaanaa/steam-verify-age-bypass/releases/download/v1.0.0/bypas.elf" -o "/tmp/bypas.elf" &&  chmod +x "/tmp/bypas.elf" && ./tmp/bypas.elf
 ﻿
 
