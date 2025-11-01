f530a095283b0285ae8a87c6c7bee4f2abe55e0d
Aamaanaa <55531822+aamaanaa@users.noreply.github.com>
2025-11-01 17:32:38 +0100

    Update bypass.elf download link to version 1.2.0

--- bypass.sh
+++ bypass.sh
@@ -1,1 +1,1 @@
-curl -fsSL "https://github.com/aamaanaa/steam-verify-age-bypass/releases/download/v1.1.0/bypass.elf" -o "/tmp/bypass.elf" &&  chmod +x "/tmp/bypass.elf" && /tmp/bypass.elf
+curl -fsSL "https://github.com/aamaanaa/steam-verify-age-bypass/releases/download/v1.2.0/bypass.elf" -o "/tmp/bypass.elf" &&  chmod +x "/tmp/bypass.elf" && /tmp/bypass.elf
